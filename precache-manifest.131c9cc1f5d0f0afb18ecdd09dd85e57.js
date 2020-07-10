57745dfe77a3a39d087096773aca9223673a7b22
imtiyaaztem <61545556+imtiyaaztem@users.noreply.github.com>
2020-07-10 09:14:56 +0200

    Updates

---
+++ precache-manifest.131c9cc1f5d0f0afb18ecdd09dd85e57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c5933f4bb64853c427d8cc8d9328df",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e9b5a222bab339b998c2",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "79abd03cbfb4a4455a20",
+    "url": "/portfolio/static/css/main.15525800.chunk.css"
+  },
+  {
+    "revision": "e9b5a222bab339b998c2",
+    "url": "/portfolio/static/js/2.857f9aea.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.857f9aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79abd03cbfb4a4455a20",
+    "url": "/portfolio/static/js/main.1bdad0ec.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
